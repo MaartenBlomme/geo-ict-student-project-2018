b90b202c93ac25a278bef6af2d72f617271d9b12
Thibeaut Formesyn <44199204+ThibeautFormesyn@users.noreply.github.com>
2019-05-15 23:31:30 +0200

    Add files via upload

--- src/REMO_analyse_meeuwen_in_elkaars_buurt.py
+++ src/REMO_analyse_meeuwen_in_elkaars_buurt.py
@@ -19,7 +19,7 @@ def ReadData(bestand):
     
     data = pd.read_csv( bestand, sep=',' ,header=(0),low_memory=False, parse_dates=['date_time'], dayfirst=True)
 
-    data = data.drop(['calc_sunlight','colony_longitude','colony_latitude','calc_corine_value','device_info_serial','project_leader','ring_code','colour_ring_code','species_code','scientific_name','catch_weight','sex','catch_location','tracking_started_at','tracking_ended_at','is_active','remarks','altitude','pressure','temperature','satellites_used','gps_fixtime','positiondop','h_accuracy','v_accuracy','x_speed','y_speed','z_speed','speed_accuracy','userflag','speed_3d','speed_2d','altitude_agl','calc_year','calc_month','calc_hour','calc_time_diff','calc_distance_diff','calc_speed_2d','calc_distance_to_colony','calc_outlier','calc_corine_legend','ESA.land.cover'], axis=1)
+    #☺data = data.drop(['calc_sunlight','colony_longitude','colony_latitude','calc_corine_value','device_info_serial','project_leader','ring_code','colour_ring_code','species_code','scientific_name','catch_weight','sex','catch_location','tracking_started_at','tracking_ended_at','is_active','remarks','altitude','pressure','temperature','satellites_used','gps_fixtime','positiondop','h_accuracy','v_accuracy','x_speed','y_speed','z_speed','speed_accuracy','userflag','speed_3d','speed_2d','altitude_agl','calc_year','calc_month','calc_hour','calc_time_diff','calc_distance_diff','calc_speed_2d','calc_distance_to_colony','calc_outlier','calc_corine_legend','ESA.land.cover'], axis=1)
     
     # dataframes per meeuw in dictionary steken
     
@@ -108,7 +108,7 @@ def BepaalBuren(afstanden, meeuwen, buffer):
 def HeatmapBuren(mergeddf, buren):
     mergeddf.reset_index(inplace = True)
     #mergeddf.rename(columns = {'date_time':'date_time1'}, inplace = True)
-    buren.rename(columns = {('Bram', 'Brigitte'): 'voorwaarde1', ('Bram', 'Claude'): 'voorwaarde2', ('Brigitte', 'Claude'): 'voorwaarde3'}, inplace = True)
+    buren.rename(columns = {('Catia', 'Eva'): 'voorwaarde1', ('Catia', 'Joanna'): 'voorwaarde2', ('Catia', 'Louise'): 'voorwaarde3', ('Catia', 'Suk-hyo'): 'voorwaarde4', ('Eva', 'Joanna'): 'voorwaarde5', ('Eva', 'Louise'): 'voorwaarde6', ('Eva', 'Suk-hyo'): 'voorwaarde7', ('Joanna', 'Louise'): 'voorwaarde8', ('Joanna', 'Suk-hyo'): 'voorwaarde9', ('Louise', 'Suk-hyo'): 'voorwaarde10'}, inplace = True)
     test = pd.merge(mergeddf, buren, how = 'outer', on = 'date_time')
     return test
 def REMO(heatmap):
@@ -118,7 +118,7 @@ def REMO(heatmap):
     #de 3 meeuwen moeten voorkomen in de geselecteerde tijdsperiode
     #enkel de kolommen bird_name, date_time en direction hebben we nodig
     #bepaalt de grote van de REMO matrix. 30 wil 3 meeuwen op 10 tijdsmomenten.
-    selectie = gesorteerd[(gesorteerd['voorwaarde1'] == True) & (gesorteerd['voorwaarde2'] == True) & (gesorteerd['voorwaarde3'] == True)]
+    selectie = gesorteerd[(gesorteerd['voorwaarde1'] == True) & (gesorteerd['voorwaarde2'] == True) & (gesorteerd['voorwaarde3'] == True) & (gesorteerd['voorwaarde4'] == True) & (gesorteerd['voorwaarde5'] == True) & (gesorteerd['voorwaarde6'] == True) & (gesorteerd['voorwaarde7'] == True) & (gesorteerd['voorwaarde8'] == True) & (gesorteerd['voorwaarde9'] == True) & (gesorteerd['voorwaarde10'] == True)]
     selectie = selectie.iloc[24654:]
     couple_columns = selectie[['bird_name','date_time', 'direction']].head(30)
     #print(couple_columns)
@@ -134,8 +134,8 @@ def REMO(heatmap):
     sns.heatmap(pivot_table, vmin = -180, vmax = 180, annot=True, fmt=".1f", linewidths=.5, square = True, cmap = 'twilight')
     return pivot_table
     
-dfdict, meeuwen = ReadData(r'C:\Users\User\Documents\2e master geografie\projectwerk geo-ict\DrieMeeuwen.csv')
-freq = '60S'
+dfdict, meeuwen = ReadData(r'C:\Users\User\Documents\2e master geografie\projectwerk geo-ict\puntenhighres.csv')
+freq = '1S'
 buffer = 500
 dfdict = SyncMeeuwen(dfdict, freq)
 mergeddf = Merge(dfdict)
