7fd5c3234e138791f3d564fac2b34a01365a34c1
Maarten Blomme <44199216+MaartenBlomme@users.noreply.github.com>
2019-05-15 09:56:51 +0200

    Update traject meeuwen.py
    Filter die dagen weglaat waar slechts 1 punt bestond. Er bestond een error op het maken van lijnen.

--- src/traject meeuwen.py
+++ src/traject meeuwen.py
@@ -5,45 +5,54 @@ Created on Wed May  8 10:18:43 2019
 @author: User
 """
 
+
+
 import pandas as pd
 import geopandas as gpd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point, LineString, shape
-def ReadData(bestand):
-    
-    data = pd.read_csv( bestand, sep=',' ,header=(0),low_memory=False, parse_dates=['date_time'], dayfirst=True)
 
-    data = data.drop(['calc_sunlight','colony_longitude','colony_latitude','calc_corine_value','device_info_serial','project_leader','ring_code','colour_ring_code','species_code','scientific_name','catch_weight','sex','catch_location','tracking_started_at','tracking_ended_at','is_active','remarks','altitude','pressure','temperature','satellites_used','gps_fixtime','positiondop','h_accuracy','v_accuracy','x_speed','y_speed','z_speed','speed_accuracy','userflag','speed_3d','speed_2d','altitude_agl','calc_year','calc_month','calc_hour','calc_time_diff','calc_distance_diff','calc_speed_2d','calc_distance_to_colony','calc_outlier','calc_corine_legend','ESA.land.cover'], axis=1)
+def ReadData(bestand):
+    kolommen = ['bird_name','date_time','longitude','latitude','direction','behaviour','calc_time_diff']
+    data = pd.read_csv(bestand, usecols = kolommen, sep=',' ,header=(0),low_memory=False, parse_dates=['date_time'], dayfirst=True)
+
     
-    # dataframes per meeuw in dictionary steken
+    return data
+
+def GetData(data):
     
     dfdict = dict(tuple(data.groupby('bird_name')))
-    meeuwen = list(dfdict.keys())
     
+    meeuwen = list(dfdict.keys())
+
     return dfdict, meeuwen
+
+data = ReadData(r'C:\Users\maart\OneDrive\Master\Projectwerk Geo-ICT\data.geo.ict.csv')
+dfdict, meeuwen = GetData(data)
 
 def lijn(dfdict):
     
+    lijnen_dfdict = dict()
     meeuwen = list(dfdict.keys())
     for meeuw in meeuwen:
         dfdict[meeuw] = dfdict[meeuw].set_index(pd.DatetimeIndex(dfdict[meeuw].date_time))
     
         geometry = [Point(xy) for xy in zip(dfdict[meeuw].longitude, dfdict[meeuw].latitude)]
-        geo_df = gpd.GeoDataFrame(dfdict[meeuw], geometry=geometry)
+        punten_df = GeoDataFrame(dfdict[meeuw], geometry=geometry)
     
         crs = {'init': 'epsg:4326'}
-        geo_df = geo_df.groupby([geo_df.index.year, geo_df.index.month, geo_df.index.day])['geometry'].apply(lambda x: LineString(x.tolist()))
-        geo_df.index.rename(['jaar', 'maand', 'dag'], inplace = True)
+        grouped = punten_df.groupby([punten_df.index.year, punten_df.index.month, punten_df.index.day]).filter(lambda x: len(x) >1 )
+        grouped = grouped.groupby([grouped.index.year, grouped.index.month, grouped.index.day])['geometry'].apply(lambda x: LineString(x.tolist()))
+        grouped.index.rename(['jaar', 'maand', 'dag'], inplace = True)
         
-       
-        geo_df = GeoDataFrame(geo_df, crs = crs, geometry='geometry')
-        geo_df.reset_index(inplace = True)
-        geo_df = geo_df.to_crs({'init': 'epsg:31370'})
+        lijnen_df = GeoDataFrame(grouped, crs = crs, geometry='geometry')
+        lijnen_df.reset_index(inplace=True)
+        lijnen_df = lijnen_df.to_crs({'init': 'epsg:31370'})
         
-        geo_df.to_file('trajecten_{}'.format(meeuw), driver = 'ESRI Shapefile')
-        print(geo_df)
-    return geo_df
-
-dfdict, meeuwen = ReadData(r'C:\Users\User\Documents\2e master geografie\projectwerk geo-ict\DrieMeeuwen.csv')
+        lijnen_df.to_file(r'C:\Users\maart\OneDrive\Master\Projectwerk Geo-ICT\Trajecten\trajecten_{}'.format(meeuw), 'ESRI Shapefile')
+        
+        lijnen_dfdict[meeuw] = lijnen_df
+    
+    return lijnen_dfdict
 
 shplijn = lijn(dfdict)
